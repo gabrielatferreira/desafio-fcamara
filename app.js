4416053a1244bcfb87e7d4bc92293302b376d409
Gabriela Trindade <byzinha88@hotmail.com>
2021-09-11 18:48:49 -0300

    ajuste cors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cors = require('cors')
+const cors = require('cors');
 const express = require('express');
 const { body, check } = require('express-validator');
 const mongoose = require('mongoose');
