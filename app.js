95c9590c392b1870a0125b3c41d36a2876ac3dbc
Gabriela Trindade <byzinha88@hotmail.com>
2021-09-11 19:09:10 -0300

    configurando porta do Heroku

--- app.js
+++ app.js
@@ -664,6 +664,4 @@ app.put("/reservas", [], async (req, res) => {
     return res.json(response)
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.PORT || 3000);
